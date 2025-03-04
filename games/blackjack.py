8eb235121a3ed32253950cc4fc45044a3e8942b6
betorres47 <btorre43@calpoly.edu>
2025-03-04 15:44:26 -0800

    first setup

---
+++ games/blackjack.py
@@ -0,0 +1,7 @@
+# - Blackjack/21
+# - user is dealt a 2 cards from a deck of cards which is sampled with replacement to allow for a random sample with duplicate cards
+# - user is
+
+
+# define the function with the float output
+def blackjack() -> float:
