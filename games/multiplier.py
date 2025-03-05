d4d6e6f4cbba53953308c8453b7673ff52264d2b
betorres47 <btorre43@calpoly.edu>
2025-03-05 11:10:15 -0800

    first setup

--- games/multiplier.py
+++ games/multiplier.py
@@ -6,12 +6,9 @@
 # - riser increases by 0.20 per second
 # - enter to stop
 # - if crash, 0, if save, float @ save
-import random
-import time
+
 import threading
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
+import time
 from scipy.stats import gamma
 
 k = 0.167                         # shape parameters for stats
@@ -19,7 +16,7 @@ theta = 102
 
 
 animation = {                   # animation frames
-    0: "[----------]",
+    0: "[##########]",
     1: "[#---------]",
     2: "[##--------]",
     3: "[###-------]",
@@ -28,8 +25,7 @@ animation = {                   # animation frames
     6: "[######----]",
     7: "[#######---]",
     8: "[########--]",
-    9: "[#########-]",
-    10: "[##########]",
+    9: "[#########-]"
 }
 
 stopped = False                 # user input default false
@@ -43,19 +39,22 @@ def get_input():                # monitor for user input using global threat sto
 
 def riser():                                            # riser game
     while True:
-        input("Are you ready? (Enter)")                 # are you ready?
+        time.sleep(1)                                   # allow for threading to take place
+        input("Ready to play? (Enter)")                 # are you ready?
 
         global stopped                                  # threading
         stopped = False                                 # reset variables
         lower_count = 0.0
         count:float = 0.0
-        bust_point = gamma.rvs(k, scale=theta, size=1)  # random crash point using gamma plot
+        sample = gamma.rvs(k, scale=theta, size=1)      # random crash point using gamma plot
+        bust_point = sample
         percent = 0
 
         input_thread = threading.Thread(target=get_input, daemon=True)          # detect for input
         input_thread.start()
 
-        for x in range(0, 101):                         # for loop to get loading bar and counter
+        for x in range(0, 101):                  # for loop to get loading bar and counter
+
             if stopped:                                 # if user decides to stop print results
                 result = count / 10
                 print("You have won!")
@@ -67,7 +66,7 @@ def riser():                                            # riser game
                 print("multiplier: 0.0")
                 break
 
-            if (count % 10) == 0:                       # add to count and lower_count when neede
+            if (count % 10) == 0:                       # add to count and lower_count when need
                 lower_count += 1
                 percent = 0
                 print(f"{lower_count}{animation[0]}{lower_count + 1.0} Win: {count / 10}")
@@ -76,6 +75,8 @@ def riser():                                            # riser game
             percent += 1
             count += 1
             time.sleep(0.5)
+
+
 
 # ADD PLAY AGAIN
 
